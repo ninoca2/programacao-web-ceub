26fc9f5ff6f92e38145a1cf59aaa1fe5fe4fc100
ninoca2 <stapmk12@gmail.com>
2024-03-21 08:58:32 -0300

    aula de js

---
+++ script.js
@@ -0,0 +1,24 @@
+/*console.log(5+5)*/
+
+let var1=5;
+let var2=30;
+
+console.log(var1 + var2);
+
+
+
+
+
+
+
+
+
+
+/*let variavel1 =0;
+
+variavel1 = "mimdepapai";
+
+const PI = 3.14;
+
+console.log(typeof(variavel1)); */
+
