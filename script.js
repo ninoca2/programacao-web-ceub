4db6841368edd2576fc2cd882bb2524c9cf8b8c0
ninoca2 <stapmk12@gmail.com>
2024-03-21 10:26:21 -0300

    for

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
+let meuArray = ["cachorro", "gato", "coelho", "calopsita", "COBRA"];
+
+for(let i = 0; i < 100; i = i + 1){
+    console.log(i);
+   // console.log(meuArray[i]);
+
+    
+
+}
+
+//console.log(meuArray[0]);
+//console.log(meuArray[1]);
+//console.log(meuArray[2]);
+//console.log(meuArray[3]);
+//console.log(meuArray[4]);
+
+
 /*console.log(5+5)*/
 
-let var1=5;
-let var2=30;
+//let var1=5;
+//let var2=4;
 
-console.log(var1 + var2);
+//let resultado2 = var1 + var2;
+
+//console.log(resultado2);
+//console.log(typeof(resultado2));
+
+//console.log(var1 + var2);
+
+//let variavelArray = [5, 4, 8, 0, -7, "abc",6.9]
+//console.log(variavelArray[5]);
+
+/*let variavelObjeto = {
+    nome: "nicole",
+    idade: 19,
+    altura: 1.90,
+    endereço: {
+        rua: "abc",
+        bairro: "ddd",
+        apartamento: {
+            numero: 614 
+        }
+    }
+}
+
+if(variavelObjeto.idade > 17) {
+    console.log("Pode entrar no meu site")
+
+} else {
+    console.log("Não pode entrar no meu site")
+}*/
+
+////console.log(variavelObjeto);
+
+//console.log(variavelObjeto["nome"]);
+//console.log(variavelArray)
 
 
 
