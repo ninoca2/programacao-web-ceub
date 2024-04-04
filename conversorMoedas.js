5aa775d80b3fa85c2881fc33b1c3a33136e98421
ninoca2 <stapmk12@gmail.com>
2024-04-04 10:29:44 -0300

    chico coins

---
+++ conversorMoedas.js
@@ -0,0 +1,36 @@
+let valoresConversao = {
+    real: {
+        dolar: 0.27,
+        euro: 0.18
+    },
+    dolar: {
+        real: 5.03,
+        euro: 0.92,
+    },
+    euro: {
+        real: 5.47,
+        dolar: 0.92
+    }
+}
+
+
+function converter() {
+    let valorUsuario = document.getElementById("valor-usuario").value;
+
+    let moedaOrigem = document.getElementById("moeda1").value;
+    let moedaDestino = document.getElementById("moeda2").value;
+
+    //console.log(valoresConversao[moedaOrigem][moedaDestino])
+
+    let conversao = valorUsuario * valoresConversao[moedaOrigem][moedaDestino];
+
+    let paragrafoResultado = document.getElementById("resultado");
+    paragrafoResultado.textContent = conversao;
+
+    console.log(conversao);
+
+    //console.log(valorUsuario);
+    //console.log(moedaOrigem);
+    //console.log(moedaDestino);
+
+}
